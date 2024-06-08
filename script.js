c3268e4297ff909b65603e4bdfc87f7ad2254e51
Vrushank <raovrushank@gmail.com>
2024-06-08 09:05:28 +0530

    Re-made it to only work on refresh

--- script.js
+++ script.js
@@ -8,29 +8,34 @@ function createSquares(n) {
       container.appendChild(square);
     }
   }
-  
-  // Create an n*n grid of squares
-
-  // for each div, let us change the background color:
-
-  //prompt message and button handling:
 
   function prompt_msg(){
     
+    //prompt
     const n = window.prompt("Enter the number of squares to create your own grid");
     createSquares(n);
+  };
+    // hovering
+    function hovering(){
       const hover = document.querySelectorAll(".square");
 
-  hover.forEach((square) => {
-    square.addEventListener("mouseover", ()=>
-    {
-      square.setAttribute("style", "background-color: black;")
+      hover.forEach((square) => {
+      square.addEventListener("mouseover", ()=>
+      {
+        square.setAttribute("style", "background-color: black;")
+      });
     });
-  });
-  }
+    };
+
+
 
   const btn = document.querySelector("#button");
   btn.addEventListener("click", prompt_msg);
+  btn.addEventListener("click", hovering);
+
+
+
+
 
   
 
